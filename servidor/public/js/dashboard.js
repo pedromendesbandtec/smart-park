4995734a21045bcb559415e053e5646978406d48
ricardomalan <ricardo.costa@bandtec.com.br>
2019-12-05 13:39:30 -0300

    Atualizações site

--- servidor/public/js/dashboard.js
+++ servidor/public/js/dashboard.js
@@ -1,3 +1,38 @@
+ocu = document.querySelector("#ocu");
+grafico_cor = document.querySelector(".grafico_cor");
+liv = document.querySelector("#liv");
+
+function buscar_dados() {
+    setTimeout(() => {
+        $.ajax({
+            url: "/dados_arduino",
+            type: "POST",
+            dataType: "HTML",
+            success: (dados) => {
+                atualizar_dados(dados);
+
+            },
+            error: () => {
+                console.error(erro);
+            }
+        });
+    }, 500);
+}
+
+buscar_dados();
+
+function atualizar_dados(dado) {
+    ocu.innerHTML = `<center style="color: red; margin-top: 10px">${dado}</center>`;
+    if (dado == 1) {
+        liv.innerHTML = '<center style="color: green; margin-top: 10px">99</center>';
+        grafico_cor.style.display = "block";
+    } else {
+        liv.innerHTML = '<center style="color: green; margin-top: 10px">100</center>';
+        grafico_cor.style.display = "none";
+    }
+    buscar_dados();
+}
+
 var x = 1600
 
 function repeticao() {
@@ -5,69 +40,8 @@ function repeticao() {
 }
 
 setInterval(() => {
-    x = x + 10;
+    x = x + 15;
     repeticao();
-}, 7000);
+}, 15000);
 
-function iniciar() {
-    repeticao()
-}
-window.onload = iniciar()
-
-
-google.charts.load('current', { 'packages': ['gauge'] });
-google.charts.setOnLoadCallback(drawChart);
-
-function drawChart() {
-
-    var data = google.visualization.arrayToDataTable([
-        ['Label', 'value'],
-        ['Automóveis', 0],
-        // ['Motos', 0],
-    ]);
-
-    var options = {
-        width: 400,
-        height: 250,
-        redFrom: 1,
-        redTo: 3,
-        // greenFrom: 4,
-        // greenTo: 6,
-        yellowFrom: 7,
-        yellowTo: 9,
-        minorTicks: 5,
-        min: 0,
-        max: 10
-    };
-
-    var chart = new google.visualization.Gauge(document.getElementById('div_grafico'));
-
-    chart.draw(data, options);
-
-
-    function buscar_dados() {
-        setTimeout(() => {
-            $.ajax({
-                url: "/dados_arduino",
-                type: "POST",
-                dataType: "HTML",
-                success: (dados) => {
-                    dado = parseInt(dados);
-                    atualizar_dados(dado);
-                },
-                error: () => {
-                    console.error(erro);
-                }
-            });
-        }, 500);
-    }
-
-    buscar_dados();
-
-    function atualizar_dados(dado) {
-        data.setValue(0, 1, dado);
-        chart.draw(data, options);
-        buscar_dados();
-    }
-
-}
+window.onload = repeticao()
