2a4beeafd35ae2c72a95ff2e2e9992e784530a4b
ricardomalan <ricardo.costa@bandtec.com.br>
2019-12-07 21:54:16 -0300

    Arq para subir o site na nuvem

---
+++ projeto-site/config/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  development: {
+    dialect: "sqlite",
+    storage: "./db.development.sqlite"
+  },
+  test: {
+    dialect: "sqlite",
+    storage: ":memory:"
+  },
+  production: {
+    username: 'usergrupo6b', //localizar no Azure
+    password: '#Gfgrupo6b', //localizar no Azure
+    database: 'bdgrupo6b', //localizar no Azure
+    host: 'srvgrupo6b.database.windows.net', //localizar no Azure
+    dialect: 'mssql',
+    xuse_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
+    },
+    pool: { 
+      max: 5,
+      min: 1,
+      acquire: 5000,
+      idle: 30000,
+      connectTimeout: 5000
+    }
+  }
+};
+ 
